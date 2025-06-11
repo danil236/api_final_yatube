57473af28375c34e5257c7972c4818f840d1f150
Danil Samko <samko2001danil@gmail.com>
2025-06-11 21:53:16 +0300

    normal_4

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-from django.db.models.constraints import UniqueConstraint, CheckConstraint
+from django.db.models.constraints import UniqueConstraint
 
 User = get_user_model()
 
